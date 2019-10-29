6566f44e452ab70aa82fcc7278510c5cbf62536b
okherozyok <yang_zhang@ruijie.com.cn>
2019-10-29 16:42:44 +0800

    增加禁输出选项

--- prohibitPing.sh
+++ prohibitPing.sh
@@ -2,31 +2,60 @@
 # ping禁止与启动，使用iptables实现。
 
 usage() {
-    echo "$0 <net interface> <stop|start>"
+    echo "`basename $0` <-I net_interface | -O> -s <stop|start>"
+	exit 1
 }
 
-if [ $# -ne 2 ] || [ "-h" = $1 ]
+if [ $# -eq 0 ]
 then
-    usage
-    exit 1
+    usage    
 fi
 
-NET_INTERFACE=$1
-SWITCH=$2
+INPUT=INPUT
+DIRECTION=
+NET_INTERFACE=
+SWITCH=
+
+while getopts :I:Os: OPTION
+do
+    case $OPTION in
+	I) NET_INTERFACE=$OPTARG
+	   DIRECTION=$INPUT
+	;;
+	O) DIRECTION=OUTPUT
+	;;
+	s) SWITCH=$OPTARG
+	;;
+	\?) usage
+	;;
+	esac
+done
 
 . ./commonFunction.sh
 
-checkNetInterface $NET_INTERFACE
-
-if [ "stop" = $SWITCH ]
+if [ $INPUT = $DIRECTION ]
 then
-    iptables -A INPUT -i $NET_INTERFACE -p icmp -j DROP
-	ifErrReturn
-elif [ "start" = $SWITCH ]
-then
-    iptables -D INPUT -i $NET_INTERFACE -p icmp -j DROP
-	ifErrReturn
-else
-    usage
-    exit 1
+    checkNetInterface $NET_INTERFACE
 fi
+
+case $SWITCH in
+    stop) if [ $INPUT = $DIRECTION ]
+	    then
+		    iptables -A INPUT -i $NET_INTERFACE -p icmp -j DROP
+	        ifErrReturn
+		else
+		    iptables -A OUTPUT -p icmp -j DROP
+	        ifErrReturn
+		fi
+    ;;
+	start) if [ $INPUT = $DIRECTION ]
+	    then
+		    iptables -D INPUT -i $NET_INTERFACE -p icmp -j DROP
+	        ifErrReturn
+		else
+		    iptables -D OUTPUT -p icmp -j DROP
+	        ifErrReturn
+		fi
+    ;;
+	*) usage
+esac
--- prohibitTcpPort.sh
+++ prohibitTcpPort.sh
@@ -2,32 +2,63 @@
 # tcp 端口禁止与启动，使用iptables实现。
 
 usage() {
-    echo "$0 <net interface> <tcp port> <stop|start>"
+    echo "`basename $0` <-I net_interface | -O> -p <tcp port> -s <stop|start>"
+	exit 1
 }
 
-if [ $# -ne 3 ] || [ "-h" = $1 ]
+if [ $# -eq 0 ]
 then
-    usage
-    exit 1
+    usage    
 fi
 
-NET_INTERFACE=$1
-TCP_PORT=$2
-SWITCH=$3
+INPUT=INPUT
+DIRECTION=
+NET_INTERFACE=
+TCP_PORT=
+SWITCH=
+
+while getopts :I:Op:s: OPTION
+do
+    case $OPTION in
+	I) NET_INTERFACE=$OPTARG
+	   DIRECTION=$INPUT
+	;;
+	O) DIRECTION=OUTPUT
+	;;
+	p) TCP_PORT=$OPTARG
+	;;
+	s) SWITCH=$OPTARG
+	;;
+	\?) usage
+	;;
+	esac
+done
 
 . ./commonFunction.sh
 
-checkNetInterface $NET_INTERFACE
-
-if [ "stop" = $SWITCH ]
+if [ $INPUT = $DIRECTION ]
 then
-    iptables -A INPUT -i $NET_INTERFACE -p tcp --dport $TCP_PORT -j DROP
-	ifErrReturn
-elif [ "start" = $SWITCH ]
-then
-    iptables -D INPUT -i $NET_INTERFACE -p tcp --dport $TCP_PORT -j DROP
-	ifErrReturn
-else
-    usage
-    exit 1
+    checkNetInterface $NET_INTERFACE
 fi
+
+case $SWITCH in
+    stop) if [ $INPUT = $DIRECTION ]
+	    then
+		    iptables -A INPUT -i $NET_INTERFACE -p tcp --dport $TCP_PORT -j DROP
+	        ifErrReturn
+		else
+		    iptables -A OUTPUT -p tcp --dport $TCP_PORT -j DROP
+	        ifErrReturn
+		fi
+    ;;
+	start) if [ $INPUT = $DIRECTION ]
+	    then
+		    iptables -D INPUT -i $NET_INTERFACE -p tcp --dport $TCP_PORT -j DROP
+	        ifErrReturn
+		else
+		    iptables -D OUTPUT -p tcp --dport $TCP_PORT -j DROP
+	        ifErrReturn
+		fi
+    ;;
+	*) usage
+esac
